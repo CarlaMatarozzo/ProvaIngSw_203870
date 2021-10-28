2fd74d537245141bf0e5bdeb07ed1e8115dfadb0
Carla <carlamata@libero.it>
2021-10-28 09:23:51 +0200

    Aggiunta Test JUnit

--- src/test/java/ProvaIntermediaTest/MyListUtilTest.java
+++ src/test/java/ProvaIntermediaTest/MyListUtilTest.java
@@ -1,4 +1,23 @@
 package ProvaIntermediaTest;
 
+import ProvaIntermedia.MyListUtil;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MyListUtilTest {
+    private MyListUtil myList;
+
+    @Test
+    public void sortTest(){
+        List<Integer> lista=new ArrayList<Integer>();
+        lista.addAll(Arrays.asList(5,9,8,6,50,30,10,7));
+        assertEquals(Arrays.asList(5,6,7,8,9,10,30,50),myList.sort(lista,0));
+        assertEquals(Arrays.asList(50,30,10,9,8,7,6,5),myList.sort(lista,1));
+        assertNotEquals(Arrays.asList(7,8,6,9,5,10,30,50),myList.sort(lista,0));
+        assertEquals(Arrays.asList(5,30,10,9,8,7,6,50),myList.sort(lista,1));
+    }
 }
